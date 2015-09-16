3bbdbb0b334cd80ce4438495f47fb666ffc98b2d
Jack Andersen <jackoalan@gmail.com>
2015-09-15 16:36:55 -1000

    Additional prep steps

--- MP1/Metroid4/Artifact Temple/MREA_2398E906/cycles_fork.py
+++ MP1/Metroid4/Artifact Temple/MREA_2398E906/cycles_fork.py
@@ -41,6 +41,28 @@ for face in to_remove:
 inward_window.to_mesh(scene.objects['MREA_2398E906_045'].data)
 scene.objects['MREA_2398E906_045'].data.update()
 
+# Remove non-rendered lights
+bpy.data.objects['LAMP_0_016'].hide_render = True
+bpy.data.objects['LAMP_0_017'].hide_render = True
+bpy.data.objects['LAMP_0_018'].hide_render = True
+bpy.data.objects['LAMP_0_019'].hide_render = True
+bpy.data.objects['LAMP_0_020'].hide_render = True
+bpy.data.objects['LAMP_0_021'].hide_render = True
+bpy.data.objects['LAMP_0_022'].hide_render = True
+bpy.data.objects['LAMP_0_023'].hide_render = True
+bpy.data.objects['LAMP_0_024'].hide_render = True
+bpy.data.objects['LAMP_0_025'].hide_render = True
+
+# Edge split modifier for floor
+scene.objects.active = scene.objects['MREA_2398E906_114']
+bpy.ops.object.modifier_add(type='EDGE_SPLIT')
+scene.objects.active = scene.objects['MREA_2398E906_115']
+bpy.ops.object.modifier_add(type='EDGE_SPLIT')
+scene.objects.active = scene.objects['MREA_2398E906_116']
+bpy.ops.object.modifier_add(type='EDGE_SPLIT')
+scene.objects.active = scene.objects['MREA_2398E906_117']
+bpy.ops.object.modifier_add(type='EDGE_SPLIT')
+
 # Save Forked File
 out_path = os.path.split(bpy.data.filepath)
 out_path = os.path.join(out_path[0], 'MREA_2398E906_cycles.blend')
